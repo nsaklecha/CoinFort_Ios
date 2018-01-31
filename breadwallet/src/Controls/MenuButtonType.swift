21e7e9d90c0d4143522b6c9a007816c1b187e88f
Nitin Saklecha <nitinsaklecha@Nitins-Mac-mini.local>
2018-01-31 19:46:32 +0530

    first commit

---
+++ breadwallet/src/Controls/MenuButtonType.swift
@@ -0,0 +1,42 @@
+//
+//  MenuButtonType.swift
+//  breadwallet
+//
+//  Created by Adrian Corscadden on 2016-11-30.
+//  Copyright © 2016 breadwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+enum MenuButtonType {
+    case security
+    case settings
+    case lock
+    case buy
+
+    var title: String {
+        switch self {
+        case .security:
+            return S.MenuButton.security
+        case .settings:
+            return S.MenuButton.settings
+        case .lock:
+            return S.MenuButton.lock
+        case .buy:
+            return S.MenuButton.buy
+        }
+    }
+
+    var image: UIImage {
+        switch self {
+        case .security:
+            return #imageLiteral(resourceName: "Shield")
+        case .settings:
+            return #imageLiteral(resourceName: "Settings")
+        case .lock:
+            return #imageLiteral(resourceName: "Lock")
+        case .buy:
+            return #imageLiteral(resourceName: "BuyBitcoin")
+        }
+    }
+}
---
+++ breadwalletTests/PaymentRequestTests.swift
@@ -0,0 +1,73 @@
+//
+//  PaymentRequestTests.swift
+//  breadwallet
+//
+//  Created by Adrian Corscadden on 2017-03-26.
+//  Copyright © 2017 breadwallet LLC. All rights reserved.
+//
+
+import XCTest
+@testable import breadwallet
+
+class PaymentRequestTests : XCTestCase {
+
+    func testEmptyString() {
+        XCTAssertNil(PaymentRequest(string: ""))
+    }
+
+    func testInvalidAddress() {
+        XCTAssertNil(PaymentRequest(string: "notandaddress"), "Payment request should be nil for invalid addresses")
+    }
+
+    func testBasicExample() {
+        let uri = "bitcoin:12A1MyfXbW6RhdRAZEqofac5jCQQjwEPBu"
+        let request = PaymentRequest(string: uri)
+        XCTAssertNotNil(request)
+        XCTAssertTrue(request?.toAddress == "12A1MyfXbW6RhdRAZEqofac5jCQQjwEPBu")
+    }
+
+    func testAmountInUri() {
+        let uri = "bitcoin:12A1MyfXbW6RhdRAZEqofac5jCQQjwEPBu?amount=1.2"
+        let request = PaymentRequest(string: uri)
+        XCTAssertNotNil(request)
+        XCTAssertTrue(request?.toAddress == "12A1MyfXbW6RhdRAZEqofac5jCQQjwEPBu")
+        XCTAssertTrue(request?.amount?.rawValue == 120000000)
+    }
+
+    func testRequestMetaData() {
+        let uri = "bitcoin:12A1MyfXbW6RhdRAZEqofac5jCQQjwEPBu?amount=1.2&message=Payment&label=Satoshi"
+        let request = PaymentRequest(string: uri)
+        XCTAssertTrue(request?.toAddress == "12A1MyfXbW6RhdRAZEqofac5jCQQjwEPBu")
+        XCTAssertTrue(request?.amount?.rawValue == 120000000)
+        XCTAssertTrue(request?.message == "Payment")
+        XCTAssertTrue(request?.label == "Satoshi")
+    }
+
+    func testExtraEqualSign() {
+        let uri = "bitcoin:12A1MyfXbW6RhdRAZEqofac5jCQQjwEPBu?amount=1.2&message=Payment=true&label=Satoshi"
+        let request = PaymentRequest(string: uri)
+        XCTAssertTrue(request?.message == "Payment=true")
+    }
+
+    func testMessageWithSpace() {
+        let uri = "bitcoin:12A1MyfXbW6RhdRAZEqofac5jCQQjwEPBu?amount=1.2&message=Payment message test&label=Satoshi"
+        let request = PaymentRequest(string: uri)
+        XCTAssertTrue(request?.message == "Payment message test")
+    }
+
+    func testPaymentProtocol() {
+        let uri = "https://www.syndicoin.co/signednoroot.paymentrequest"
+        let request = PaymentRequest(string: uri)
+        XCTAssertTrue(request?.type == .remote)
+
+        let promise = expectation(description: "Fetch Request")
+        request?.fetchRemoteRequest(completion: { newRequest in
+            XCTAssertNotNil(newRequest)
+            promise.fulfill()
+        })
+
+        waitForExpectations(timeout: 5.0) { error in
+            XCTAssertNil(error)
+        }
+    }
+}
